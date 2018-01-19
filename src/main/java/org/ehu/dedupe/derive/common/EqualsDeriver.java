098299f098657f1d4c4f5c026d761aec0ef793de
francetem <francetem@hotmail.com>
2018-01-19 14:39:06 +0100

    Fix NPE in EqualsDeriver

--- src/main/java/org/ehu/dedupe/derive/common/EqualsDeriver.java
+++ src/main/java/org/ehu/dedupe/derive/common/EqualsDeriver.java
@@ -4,11 +4,12 @@ package org.ehu.dedupe.derive.common;
 import org.ehu.dedupe.derive.ReflectionDeriver;
 import org.ehu.dedupe.derive.SimpleResult;
 
+import java.util.Objects;
 import java.util.function.Function;
 
 public class EqualsDeriver<S> extends ReflectionDeriver<Boolean, S> {
 
     public EqualsDeriver(String propertyName, Function<S, Object> getter) {
-        super(propertyName, (left, right) -> new SimpleResult<>(getter.apply(left).equals(getter.apply(right))));
+        super(propertyName, (left, right) -> new SimpleResult<>(Objects.equals(getter.apply(left), getter.apply(right))));
     }
 }
--- src/test/java/org/ehu/dedupe/derive/common/EqualsDeriverTest.java
+++ src/test/java/org/ehu/dedupe/derive/common/EqualsDeriverTest.java
@@ -14,4 +14,13 @@ public class EqualsDeriverTest {
         assertTrue(equalsDeriver.calculate("any", "any", null).getResult().process());
         assertFalse(equalsDeriver.calculate("any", "none", null).getResult().process());
     }
+
+    @Test
+    public void testDeriverOnNull() throws Exception {
+        EqualsDeriver<String> equalsDeriver = new EqualsDeriver<>("any", t -> "none".equals(t) ? null : t);
+
+        assertFalse(equalsDeriver.calculate("any", "none", null).getResult().process());
+        assertFalse(equalsDeriver.calculate("none", "any", null).getResult().process());
+        assertTrue(equalsDeriver.calculate("none", "none", null).getResult().process());
+    }
 }
