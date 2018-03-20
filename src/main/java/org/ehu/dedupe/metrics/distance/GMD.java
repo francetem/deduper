03a714433aeb5249061d26affe1c65fb6abb9eb1
francetem <francetem@hotmail.com>
2018-03-20 12:10:32 +0100

    improving GMD

--- src/main/java/org/ehu/dedupe/metrics/distance/GMD.java
+++ src/main/java/org/ehu/dedupe/metrics/distance/GMD.java
@@ -1,8 +1,6 @@
 package org.ehu.dedupe.metrics.distance;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
 import org.ehu.dedupe.data.Buckets;
 
 import java.math.BigDecimal;
@@ -10,42 +8,29 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class GMD {
 
-    private final List<Pair<Set, Set>> splits = new ArrayList<>();
-    private final List<Pair<Set, Set>> merges = new ArrayList<>();
-
-    <T> BigDecimal unitCostFunction(Set<T> left, Set<T> right) {
-        if (left.isEmpty() || right.isEmpty()) {
-            return BigDecimal.ZERO;
-        }
-        return BigDecimal.ONE;
-    }
+    private final Collection<Collection<Collection>> splits = new ArrayList<>();
+    private final Collection<Collection<Collection>> merges = new ArrayList<>();
 
     public <I> BigDecimal cost(Buckets<I> er, Buckets<I> gt) {
         return cost(er, gt, this::split, this::merge);
     }
 
-    private <I> BigDecimal merge(Set<I> left, Set<I> right) {
-        return update(left, right, merges);
+    private <I> BigDecimal merge(Collection<Collection<I>> clusters) {
+        return update(clusters);
     }
 
-    private <I> BigDecimal split(Set<I> left, Set<I> right) {
-        return update(left, right, splits);
+    private <I> BigDecimal split(Collection<Collection<I>> clusters) {
+        return update(clusters);
     }
 
-    private <I> BigDecimal update(Set<I> left, Set<I> right, List<Pair<Set, Set>> pairs) {
-        BigDecimal result = unitCostFunction(left, right);
-        if (!BigDecimal.ZERO.equals(result)) {
-            pairs.add(new ImmutablePair<>(left, right));
-        }
-        return result;
+    private <I> BigDecimal update(Collection<Collection<I>> clusters) {
+        return BigDecimal.valueOf(clusters.size() - 1);
     }
 
     /**
@@ -59,44 +44,64 @@ public class GMD {
      * @param <I>    type of elements
      * @return The GMD cost from er to gt
      */
-    public <I> BigDecimal cost(Buckets<I> er, Buckets<I> gt, BiFunction<Set<I>, Set<I>, BigDecimal> fSplit, BiFunction<Set<I>, Set<I>, BigDecimal> fMerge) {
+    public <I> BigDecimal cost(Buckets<I> er, Buckets<I> gt, Function<Collection<Collection<I>>, BigDecimal> fSplit, Function<Collection<Collection<I>>, BigDecimal> fMerge) {
 
-        Map<Integer, Set<Set<I>>> merging = new HashMap<>();
-        BigDecimal cost = BigDecimal.ZERO;
+        Map<Integer, Collection<Collection<I>>> merging = new HashMap<>();
+        Collection<Collection<Collection<I>>> splitting = new ArrayList<>();
         for (Set<I> es : er.clusters()) {
-            Set<Integer> tIds = new HashSet<>();
-            for (I t : es) {
-                if (gt.contains(t)) {
-                    Collection<Integer> containing = gt.containing(t);
-                    tIds.addAll(containing);
+            Set<Integer> tIds = intersectingIds(gt, es);
+
+            if (tIds.size() > 1) {
+                Collection<Collection<I>> currentSplits = new ArrayList<>();
+                for (Integer id : tIds) {
+                    Set<I> set = gt.get(id);
+                    Collection<I> intersection = CollectionUtils.intersection(set, es);
+                    currentSplits.add(intersection);
                 }
+                splitting.add(currentSplits);
             }
-            Set<I> splitting = new HashSet<>(es);
+
             for (Integer id : tIds) {
-                Collection<I> intersection = CollectionUtils.intersection(splitting, gt.get(id));
-                splitting = new HashSet<>(CollectionUtils.subtract(splitting, intersection));
-                cost = cost.add(fSplit.apply(new HashSet<>(intersection), splitting));
-                if (!merging.containsKey(id)) {
-                    merging.put(id, new HashSet<>());
+                Set<I> set = gt.get(id);
+                Collection<I> intersection = CollectionUtils.intersection(set, es);
+                if (intersection.size() > 0 && intersection.size() < set.size()) {
+                    if (!merging.containsKey(id)) {
+                        merging.put(id, new ArrayList<>());
+                    }
+                    merging.get(id).add(intersection);
                 }
-                merging.get(id).add(new HashSet<I>(intersection));
             }
         }
-        for (Map.Entry<Integer, Set<Set<I>>> toMerge : merging.entrySet()) {
-            Iterator<Set<I>> iterator = toMerge.getValue().iterator();
-            Set<I> merge = new HashSet<>(iterator.next());
-            while (iterator.hasNext()) {
-                cost = cost.add(fMerge.apply(merge, iterator.next()));
-            }
-        }
-        return cost;
+
+        merges.addAll(new HashSet(merging.values()));
+        splits.addAll(new HashSet(splitting));
+        BigDecimal mergeCost = merging.values().stream()
+                .map(fMerge)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal splitCost = splitting.stream()
+                .map(fSplit)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        return mergeCost.add(splitCost);
     }
 
-    public List<Pair<Set, Set>> getSplits() {
+    public <I> Set<Integer> intersectingIds(Buckets<I> gt, Set<I> es) {
+        Set<Integer> tIds = new HashSet<>();
+        for (I t : es) {
+            if (gt.contains(t)) {
+                Collection<Integer> containing = gt.containing(t);
+                tIds.addAll(containing);
+            }
+        }
+        return tIds;
+    }
+
+    public Collection<Collection<Collection>> getSplits() {
         return splits;
     }
 
-    public List<Pair<Set, Set>> getMerges() {
+    public Collection<Collection<Collection>> getMerges() {
         return merges;
     }
 }
--- src/test/java/org/ehu/dedupe/metrics/distance/GMDTest.java
+++ src/test/java/org/ehu/dedupe/metrics/distance/GMDTest.java
@@ -5,10 +5,12 @@ import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class GMDTest {
@@ -24,5 +26,24 @@ public class GMDTest {
 
         BigDecimal cost = gmd.cost(Buckets.from(Arrays.asList(cluster1, cluster2)), Buckets.from(Arrays.asList(cluster3, cluster4)));
         assertTrue(cost.compareTo(BigDecimal.valueOf(4)) == 0);
+    }
+
+    @Test
+    public void testMerges() throws Exception {
+        GMD gmd = new GMD();
+        Set<String> cluster1 = Stream.of("r1").collect(Collectors.toSet());
+        Set<String> cluster2 = Stream.of("r2").collect(Collectors.toSet());
+        Set<String> cluster3 = Stream.of("r3").collect(Collectors.toSet());
+
+        Set<String> cluster4 = Stream.of("r1","r2","r3").collect(Collectors.toSet());
+
+        Buckets<String> bucket1 = Buckets.from(Arrays.asList(cluster1, cluster2, cluster3));
+        Buckets<String> bucket2 = Buckets.from(Collections.singletonList(cluster4));
+
+        gmd.cost(bucket1, bucket2);
+        assertEquals(gmd.getMerges().size(), 1);
+
+        gmd.cost(bucket2, bucket1);
+        assertEquals(gmd.getSplits().size(), 1);
     }
 }
